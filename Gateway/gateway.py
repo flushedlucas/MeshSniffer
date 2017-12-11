389f5756e52b92a9dc073f970721bbff77b26c84
Lucas Vieira <lucas.filipe.vieira@gmail.com>
2017-12-11 12:07:23 -0300

    Commit Inicial

---
+++ Gateway/gateway.py
@@ -0,0 +1,23 @@
+def receivedMessage(origem, msg):
+    pass
+
+def sendSingle(dest, msg):
+    pass
+
+def sendBroadcast(msg):
+    pass
+
+def removeConnection(connID):
+    pass
+
+def readJsonObject():
+    pass
+
+def handleMessage(fromID, json):
+    pass
+
+def sendNodeSync(fromID, request):
+    pass
+
+def nodeTime(): #Retorna o tempo do nó
+    pass
